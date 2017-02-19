3ca73ba81d6f57d58a21dc00c944e30e73a40ca1
Smile <sisitymiteva@yahoo.com>
2017-02-19 16:55:26 +0200

    header structure done

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -22,11 +22,47 @@
 
 </head>
 <body>
-    <div class="container">
-        @section('header')
-            This is the master header.
-        @show
-    </div>
+    @section('header')
+    <header>
+        <div class="wide-header container-fluid">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-6 logo">
+                        <img src="/images/logo.png" alt="logo" />
+                    </div>
+                    <div class="col-sm-6 contanct">
+                        <div class="pull-right">Contact us on<br />+359 883 472 597</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="main-menu container">
+            <nav>
+                <ul>
+                    <li>
+                        <a href="#">Home</a>
+                    </li>
+                    <li>
+                        <a href="#">Services</a>
+                    </li>
+                    <li>
+                        <a href="#">Blog</a>
+                    </li>
+                    <li>
+                        <a href="#">Contacts</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+        <div class="header-image container-fluid">
+            <h1>Header Content</h1>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="none">
+                <polygon class="svg--sm" fill="#efefef" points="0,0 30,100 65,21 90,100 100,75 100,100 0,100"/>
+                <polygon class="svg--lg" fill="#efefef" points="0,0 15,100 33,21 45,100 50,75 55,100 72,20 85,100 95,50 100,80 100,100 0,100" />
+            </svg>
+        </div>
+    </header>
+    @show
 
     <div class="container">
         @yield('content')
@@ -39,7 +75,9 @@
     </div>
     
         @section('footer')
-          <footer>foooooot</footer>
+          <footer>
+  
+          </footer>
         @show
   
     <!-- Scripts -->
