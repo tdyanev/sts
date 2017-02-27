f6fafbed36d90f94f1ee41e1d718124d1a6cc863
Tihomir Yanev <tdyanev@gmail.com>
2017-02-27 18:07:39 +0200

    Delete old package

--- app/Http/MyPackage/view/text.blade.php
+++
@@ -1,4 +0,0 @@
-<div class="form-group">
-    <label for="input-{{ $name }}">{{ $label }}</label>
-    <textarea id="input-{{ $name }}" name="{{ $name }}" class="form-control">{{ $value }}</textarea>
-</div>
