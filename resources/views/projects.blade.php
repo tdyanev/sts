20b4c0698b830bb3f893f55a78aa16fed6c0619c
Smile <sisitymiteva@yahoo.com>
2017-02-28 13:14:57 +0200

    Different pages created.

---
+++ resources/views/projects.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.master')
+
+@section('content')
+
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-8 col-xs-12">
+                
+            </div>
+            <div class="col-sm-4 col-xs-12">
+            
+            </div>
+        </div>
+    </div>
+</section>
+
+@endsection
