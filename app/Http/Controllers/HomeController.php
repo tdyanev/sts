e83be813b6134dce074077a516cdedbf5702c1d5
Tihomir Yanev <tdyanev@gmail.com>
2017-08-18 20:14:53 +0300

    Add success message in lang/

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -31,13 +31,18 @@ class HomeController extends Controller
     public function contacts_send(Request $request) {
         $receiver = env('MAIL_USERNAME');
 
-        \Mail::to($receiver)->send(new ContactSend([
+        \Mail::to($receiver)
+            ->send(new ContactSend([
 
-            'name' => $request->name,
-            'email' => $request->email,
-            'subject' => $request->subject,
-            'message' => $request->message,
-        ]));
+                'name' => $request->name,
+                'email' => $request->email,
+                'subject' => $request->subject,
+                'message' => $request->message,
+            ]));
+
+        $request->session()
+            ->flash('mail_status', count(Mail::failures()) > 0);
+
 
         return back();
     }
